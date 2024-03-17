4a791ca5c32d25f682676634755c04f1360fdcc4
Georgefarah825 <162799743+Georgefarah825@users.noreply.github.com>
2024-03-17 20:12:23 +0300

    Update loadingscreen.dart

--- lib/screens/loadingscreen.dart
+++ lib/screens/loadingscreen.dart
@@ -1,1 +1,66 @@
 
+
+
+
+
+
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:climaweather/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+import 'location_screen.dart';
+import 'package:climaweather/services/weather.dart';
+
+
+
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+
+
+double? longtude;
+double? latitude;
+
+  @override
+  void initState(){
+    super.initState();
+getlocationdata();
+
+
+  }
+
+//video002getlocationMethod
+  void getlocationdata() async {
+
+    WeatherModel wethermodel = WeatherModel();
+
+
+    var weatherdata = await wethermodel.getlocationweather();
+
+
+
+Navigator.push(context, MaterialPageRoute(builder: (context){
+  return LocationScreen(locationweather: weatherdata,);
+
+}));
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+body: Center(child:
+  SpinKitDoubleBounce(
+    color: Colors.white,
+    size: 100.0,
+  )
+  ,),
+    );
+  }
+}
